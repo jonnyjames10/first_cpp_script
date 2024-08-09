fe61c493d7c3cdc15e5994c5433bce9384f3a424
Jonny Streatfeild-James <jonnysj8@gmail.com>
2024-08-09 16:27:24 +0100

    Removed unnecessary lines to be in the same for loop

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -17,19 +17,17 @@ bool check_win(char board[3][3], char player) {
     // Check all diagonals
     if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
         return true;
-    } else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+    } 
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
         return true;
     } 
 
-    // Check all verticals
     for (int i = 0; i < 3; i++) {
+        // Check all verticals
         if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
             return true;
         }
-    }
-
-    // Check all horizontals
-    for (int i = 0; i < 3; i++) {
+        // Check all horizontals
         if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
             return true;
         }
