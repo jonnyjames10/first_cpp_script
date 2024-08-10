c723e329e351b0329382b46721bdd113802c5447
Jonny Streatfeild-James <jonnysj8@gmail.com>
2024-08-10 17:11:36 +0100

    Class methods implemented

--- bank_account.cpp
+++ bank_account.cpp
@@ -12,15 +12,17 @@ class account {
         account(std::string accountName, double startingBalance) : name(accountName), balance(startingBalance) {}
 
         void deposit(double amount) {
-            
+            balance += amount;
         }
 
         void withdraw(double amount) {
-
+            if (balance > 0 && amount < balance) {
+                balance -= amount;
+            }
         }
 
         float get_balance() {
-
+            return balance;
         }
 };
 
